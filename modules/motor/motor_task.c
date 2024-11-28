5be8ec09b498be669eef598e57219228dfb4bea3
RMDT46HCH <1702921821@qq.com>
2024-11-28 21:34:29 +0800

    small_change

--- modules/motor/motor_task.c
+++ modules/motor/motor_task.c
@@ -1,12 +1,14 @@
 #include "motor_task.h"
 #include "dji_motor.h"
 #include "servo_motor.h"
+#include "bsp_dwt.h"
+static uint8_t cnt = 0; //设定不同电机的任务频率
 
 void MotorControlTask()
 {
-    // static uint8_t cnt = 0; 设定不同电机的任务频率
-    // if(cnt%5==0) //200hz
-    // if(cnt%10==0) //100hz
+    cnt=DWT_GetTimeline_s();
+    if(cnt%10==0) //100hz
     DJIMotorControl();
+    if(cnt%50==0) //20hz
     ServeoMotorControl();
 }
