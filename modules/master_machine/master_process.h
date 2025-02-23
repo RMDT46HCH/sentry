cd27d117065fd2cd502d0ed7b7f09852e795cd8a
RMDT46HCH <1702921821@qq.com>
2025-02-23 20:23:36 +0800

    小调整

--- modules/master_machine/master_process.h
+++ modules/master_machine/master_process.h
@@ -159,7 +159,7 @@ void NavSetMessage(float vx, float vy, float yaw,uint8_t occupation,
 					uint16_t self_sentry_HP,uint16_t self_infantry_HP,uint16_t self_hero_HP,
 					uint16_t enermy_sentry_HP,uint16_t enermy_infantry_HP,uint16_t enermy_hero_HP,
                     uint16_t remain_time,uint16_t remain_bullet,uint8_t game_progress
-					)
+					);
 
 /*更新发送数据帧，并计算发送数据帧长度*/
 void get_protocol_send_Vision_data(
